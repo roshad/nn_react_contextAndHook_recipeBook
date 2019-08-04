6acfae1d62c5955bc22ccd0b29f37f9cf57e8c74
roshad <roshad87a9@gmail.com>
2019-08-04 19:22:21 +0800

    歪的

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <BookList />
+        </div>
+    );
 }
 
+function Navbar() {
+    return (
+        <nav className='Navbar'>
+            <h1>something</h1>
+            <ul>
+                <li>thing1</li>
+                <li>thing2</li>
+                <li>thing3</li>
+            </ul>
+        </nav>
+    );
+}
+
+function BookList() {
+    return (
+        <ul>
+            <li>book1</li>
+            <li>book2</li>
+            <li>book3</li>
+        </ul>
+    );
+}
 export default App;
